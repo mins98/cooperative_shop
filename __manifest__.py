8f64bdb01703ce7e2d526f7d08e5ff463f2528b1
Miguel Nava <minsor2010@hotmail.com>
2023-07-31 13:05:44 -0400

    [ADD] New Module

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Cooperative Shoop',
+    "summary":"""Module to handle the work of volunteers""",
+    "description":"""Module to handle:
+        - Volunteers
+        - Organizations
+        - Shoop
+    """,
+    'license':'OPL-1',
+    'author': 'Miguel',
+    'website':'www.odoo.com',
+    'category': 'Custom Modules/Cooperative',
+    'version': '0.1',
+    'depends':  ['base'],
+    'demo':  [''],
+    'data':  [''],
+    'application': True,
+}
